d2f2bc6494cf40532e54007f656227c2ede38823
Emerald <emerald_mg@yahoo.com>
2020-05-03 14:57:58 -0500

    Assignment Complete

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -76,7 +77,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -123,6 +124,21 @@ public class JobData {
             System.out.println("Failed to load job data");
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList();
+        for (HashMap<String, String> job : allJobs) {
+            for (Map.Entry<String, String> section : job.entrySet()) {
+                String toSearch = section.getValue().toLowerCase();
+                if (toSearch.contains(value.toLowerCase()) && !jobs.contains(job)) {
+                    jobs.add(job);
+                }
+            }
+        }
+        return jobs;
     }
 
 }
